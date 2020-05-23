b8e267af879f30f754a302d483dcf2d7b5f2677c
binthafra <binthafra@gmail.com>
2020-05-23 17:02:34 +0600

    Python Basics

---
+++ Pyhton Basics/DataTypes.py
@@ -0,0 +1,31 @@
+# Fundamentals Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+# Classes -> custom Types
+
+# specialized Data Types
+# Eundumwntals Data type
+
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+
+
+print(2/4)
+print(2//4)
+print(4 % 2)
+
+
+# Math Functions
+print(round(3.1))
+
+
+# bin ,Base, complex
+print(bin(5))  # 0b represent binary
+print(int('0b101', 2))  # here 2 is Base
