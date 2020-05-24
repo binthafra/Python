74b8148c284cc1430a1a247db01e25d252e1a59e
binthafra <binthafra@gmail.com>
2020-05-24 17:56:04 +0600

    Python Basics

---
+++ Pyhton Basics/String Indexs.py
@@ -0,0 +1,19 @@
+python = 'I am PYHTON'
+#         01234567890
+
+# [start: stop]
+print(python[0:11])
+# [start: stop:StepOver]
+print(python[0:11:2])
+
+
+print(python[1:4])
+print(python[1:])  # start at the frst
+print(python[:])
+print(python[:6])
+print(python[1:100])
+print(python[-1])  # start at the end
+print(python[-4])
+print(python[:-3])
+print(python[-3:])
+print(python[::-1])  # reverse
