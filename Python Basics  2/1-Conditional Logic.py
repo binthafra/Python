ef7005caa0211bd71e68ebaf6c5042e6c4ccc43f
binthafra <binthafra@gmail.com>
2020-05-28 00:44:52 +0600

    Python Basics 2

---
+++ Python Basics  2/1-Conditional Logic.py
@@ -0,0 +1,10 @@
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print("your are old enough to drive")
+# elif is_licenced:
+#     print("you can drive nw")
+else:
+    print("check")
+print("okok")
