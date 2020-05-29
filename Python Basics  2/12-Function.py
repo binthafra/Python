7cbb9fc44e03d6b0b2600aa5154cebef7ffb0b09
binthafra <binthafra@gmail.com>
2020-05-29 19:33:26 +0600

    Python Basics 2

---
+++ Python Basics  2/12-Function.py
@@ -0,0 +1,6 @@
+def say_hello(name, emoji):
+    print(f'helloooo {name} {emoji}')
+
+
+# arguments
+say_hello('Afra', 's')
--- Python Basics  2/9-while loop.py
+++ Python Basics  2/9-while loop.py
@@ -2,3 +2,4 @@ i = 0
 while i < 50:
     print(i)
     i = i+1
+    i += 1
