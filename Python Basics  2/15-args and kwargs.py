b311690f0c6e7ca11dd4d5c526280e12c8d09779
binthafra <binthafra@gmail.com>
2020-05-30 16:25:52 +0600

    Python Basics 2

---
+++ Python Basics  2/15-args and kwargs.py
@@ -0,0 +1,21 @@
+def super_func(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    return sum(args)
+
+
+print(super_func(1, 2, 3, 4, 5, num1=5, num2=10))
+
+# 2
+
+
+def super_func(*args, **kwargs):
+    total = 0
+    for items in kwargs.values():
+        total += items
+    print(args)
+    print(kwargs)
+    return sum(args) + total
+
+
+print(super_func(1, 2, 3, 4, 5, num1=5, num2=10))
